ca12fce4e5d8ffec0571f470778eb988dd68bd38
Louie <louiemeeks@icloud.com>
2024-04-10 14:25:23 +0100

    added validation tests for direct payment and payment request forms

--- payapp/tests.py
+++ payapp/tests.py
@@ -1,3 +1,103 @@
 from django.test import TestCase
+from .forms import DirectPaymentForm, PaymentRequestForm
+from register.models import User
 
-# Create your tests here.
+
+class TestDirectPaymentForm(TestCase):
+    def setUp(self):
+        user1 = User(username='test_user_1',
+                    first_name='test_user',
+                    last_name='1',
+                    email='test_user_1@email.com',
+                    phone_number='07123456789',
+                    currency='£',
+                    password='Password123!'
+                    )
+        user1.create_user()
+        user2 = User(username='test_user_2',
+                    first_name='test_user',
+                    last_name='2',
+                    email='test_user_2@email.com',
+                    phone_number='07987654321',
+                    currency='£',
+                    password='Password123!'
+                    )
+        user2.create_user()
+
+    def test_email_blank_validation(self):
+        form = DirectPaymentForm(data={'receiver_email': ''})
+        self.assertEqual(form.errors['receiver_email'], ['This field is required.'])
+        self.assertFalse(form.is_valid())
+
+    def test_email_exists_validation(self):
+        form = DirectPaymentForm(data={'receiver_email': 'test_user_3@email.com'})
+        self.assertEqual(form.errors['__all__'], ['No user exists with that email'])
+        self.assertFalse(form.is_valid())
+
+    def test_emails_same_validation(self):
+        form = DirectPaymentForm(data={'receiver_email': 'test_user_1@email.com', 'sender_email': 'test_user_1@email.com'})
+        self.assertEqual(form.errors['__all__'], ['You cannot send money to yourself'])
+        self.assertFalse(form.is_valid())
+
+    def test_reference_blank_validation(self):
+        form = DirectPaymentForm(data={'reference': ''})
+        self.assertEqual(form.errors['receiver_email'], ['This field is required.'])
+        self.assertFalse(form.is_valid())
+
+    def test_amount_blank_validation(self):
+        form = DirectPaymentForm(data={'amount': None})
+        self.assertEqual(form.errors['amount'], ['This field is required.'])
+        self.assertFalse(form.is_valid())
+
+    def test_user_has_amount_validation(self):
+        form = DirectPaymentForm(data={'receiver_email': 'test_user_1@email.com', 'sender_email': 'test_user_2@email.com', 'amount': 10000})
+        self.assertEqual(form.errors['__all__'], ['You do not contain the required funds to make this payment'])
+        self.assertFalse(form.is_valid())
+
+
+class TestPaymentRequestForm(TestCase):
+    def setUp(self):
+        user1 = User(username='test_user_1',
+                     first_name='test_user',
+                     last_name='1',
+                     email='test_user_1@email.com',
+                     phone_number='07123456789',
+                     currency='£',
+                     password='Password123!'
+                     )
+        user1.create_user()
+        user2 = User(username='test_user_2',
+                     first_name='test_user',
+                     last_name='2',
+                     email='test_user_2@email.com',
+                     phone_number='07987654321',
+                     currency='£',
+                     password='Password123!'
+                     )
+        user2.create_user()
+
+    def test_email_blank_validation(self):
+        form = PaymentRequestForm(data={'receiver_email': ''})
+        self.assertEqual(form.errors['receiver_email'], ['This field is required.'])
+        self.assertFalse(form.is_valid())
+
+    def test_email_exists_validation(self):
+        form = PaymentRequestForm(data={'receiver_email': 'test_user_3@email.com'})
+        print(form.errors)
+        self.assertEqual(form.errors['__all__'], ['No user exists with that email'])
+        self.assertFalse(form.is_valid())
+
+    def test_emails_same_validation(self):
+        form = PaymentRequestForm(data={'receiver_email': 'test_user_1@email.com', 'sender_email': 'test_user_1@email.com'})
+        self.assertEqual(form.errors['__all__'], ['You cannot request money from yourself'])
+        self.assertFalse(form.is_valid())
+
+    def test_reference_blank_validation(self):
+        form = PaymentRequestForm(data={'reference': ''})
+        self.assertEqual(form.errors['receiver_email'], ['This field is required.'])
+        self.assertFalse(form.is_valid())
+
+    def test_amount_blank_validation(self):
+        form = PaymentRequestForm(data={'amount': None})
+        self.assertEqual(form.errors['amount'], ['This field is required.'])
+        self.assertFalse(form.is_valid())
--- register/tests.py
+++ register/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from .forms import RegisterForm
-from register.models import User
+from .models import User
 from payapp.converter import Currencies
 
 
@@ -19,83 +19,101 @@ class TestRegisterForm(TestCase):
     def test_username_blank_validation(self):
         form = RegisterForm(data={'username': ''})
         self.assertEqual(form.errors['username'], ['This field is required.'])
+        self.assertFalse(form.is_valid())
 
     def test_username_exists_validation(self):
         form = RegisterForm(data={'username': 'test_user_1'})
         self.assertEqual(form.errors['username'], ['Username already in use'])
+        self.assertFalse(form.is_valid())
 
     def test_first_name_blank_validation(self):
         form = RegisterForm(data={'first_name': ''})
         self.assertEqual(form.errors['first_name'], ['This field is required.'])
+        self.assertFalse(form.is_valid())
 
     def test_last_name_exists_validation(self):
         form = RegisterForm(data={'last_name': ''})
         self.assertEqual(form.errors['last_name'], ['This field is required.'])
+        self.assertFalse(form.is_valid())
 
     def test_email_blank_validation(self):
         form = RegisterForm(data={'email': ''})
         self.assertEqual(form.errors['email'], ['This field is required.'])
+        self.assertFalse(form.is_valid())
 
     def test_email_exists_validation(self):
         form = RegisterForm(data={'email': 'test_user_1@email.com'})
         self.assertEqual(form.errors['email'], ['Email already in use'])
+        self.assertFalse(form.is_valid())
 
     def test_phone_number_blank_validation(self):
         form = RegisterForm(data={'phone_number': ''})
         self.assertEqual(form.errors['phone_number'], ['This field is required.'])
+        self.assertFalse(form.is_valid())
 
     def test_phone_number_exists_validation(self):
         form = RegisterForm(data={'phone_number': '07123456789'})
         self.assertEqual(form.errors['phone_number'], ['Phone Number already in use'])
+        self.assertFalse(form.is_valid())
 
     def test_currency_blank_validation(self):
         form = RegisterForm(data={'currency': ''})
         self.assertEqual(form.errors['currency'], ['This field is required.'])
+        self.assertFalse(form.is_valid())
 
     def test_is_valid_currency_validation(self):
         form = RegisterForm(data={'currency': 'GBP'})
         currency = form.data.get('currency')
         self.assertTrue((Currencies.EUR == currency) or (Currencies.GBP == currency) or (Currencies.USD == currency))
+        self.assertFalse(form.is_valid())
 
     def test_is_invalid_currency_validation(self):
         form = RegisterForm(data={'currency': 'AUD'})
         currency = form.data.get('currency')
         self.assertFalse((Currencies.EUR == currency) or (Currencies.GBP == currency) or (Currencies.USD == currency))
+        self.assertFalse(form.is_valid())
 
     def test_password_1_blank_validation(self):
         form = RegisterForm(data={'password1': ''})
         self.assertEqual(form.errors['password1'], ['This field is required.'])
+        self.assertFalse(form.is_valid())
 
     def test_password_1_length_validation(self):
         form = RegisterForm(data={'password1': 'Pa1!'})
         self.assertEqual(form.errors['password1'], ['Password must contain at least 8 characters'])
+        self.assertFalse(form.is_valid())
 
     def test_password_1_uppercase_validation(self):
         form = RegisterForm(data={'password1': 'password123!'})
         self.assertEqual(form.errors['password1'], ['Password must contain at least 1 uppercase character'])
+        self.assertFalse(form.is_valid())
 
     def test_password_1_lowercase_validation(self):
         form = RegisterForm(data={'password1': 'PASSWORD123!'})
         self.assertEqual(form.errors['password1'], ['Password must contain at least 1 lowercase character'])
+        self.assertFalse(form.is_valid())
 
     def test_password_1_digit_validation(self):
         form = RegisterForm(data={'password1': 'Password!'})
         self.assertEqual(form.errors['password1'], ['Password must contain at least 1 number character'])
+        self.assertFalse(form.is_valid())
 
     def test_password_1_special_validation(self):
         form = RegisterForm(data={'password1': 'Password123'})
         self.assertEqual(form.errors['password1'], ['Password must contain at least 1 special character'])
+        self.assertFalse(form.is_valid())
 
     def test_password_1_space_validation(self):
         form = RegisterForm(data={'password1': 'Password 123!'})
         self.assertEqual(form.errors['password1'], ['Password must not contain any whitespace characters'])
+        self.assertFalse(form.is_valid())
 
     def test_password_2_blank_validation(self):
         form = RegisterForm(data={'password2': ''})
         self.assertEqual(form.errors['password2'], ['This field is required.'])
+        self.assertFalse(form.is_valid())
 
     def test_password_2_match_validation(self):
         form = RegisterForm(data={'password1': 'Password123!', 'password2': 'Password456!'})
         self.assertEqual(form.errors['password2'], ['The two password fields didn’t match.'])
-
-
+        self.assertFalse(form.is_valid())
