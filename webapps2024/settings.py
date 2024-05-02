fea7ec349448870edd8a9d68f5af6c961cbb052b
Louie <louiemeeks@icloud.com>
2024-05-02 22:27:36 +0100

    fix

--- webapps2024/settings.py
+++ webapps2024/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms'
+    'crispy_forms',
     "phonenumber_field",
     "register",
     "payapp",
